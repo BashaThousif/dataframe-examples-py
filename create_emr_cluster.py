403be42925eecdac27f8ae5b45bacecdcfb5ee16
Thousif <mullathousifbasha@gmail.com>
2021-05-26 20:25:55 +0530

    Initial commit

--- create_emr_cluster.py
+++ create_emr_cluster.py
@@ -13,9 +13,9 @@ def lambda_handler(event, context):
         'Ec2KeyName': 'spark',
         'KeepJobFlowAliveWhenNoSteps': True,
         'TerminationProtected': False,
-        'Ec2SubnetId': 'subnet-b51b2fd3',
-        'EmrManagedMasterSecurityGroup': 'sg-00e712ec1b09f676e',
-        'EmrManagedSlaveSecurityGroup':  'sg-0e9ebbfd18d0669a8'
+        'Ec2SubnetId': 'subnet-076d7361',
+        'EmrManagedMasterSecurityGroup': 'sg-079f080de0b7edc31',
+        'EmrManagedSlaveSecurityGroup':  'sg-06693caa91f680db3'
     }
 
     configurations = [
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
 
     response = client.run_job_flow(
         Name='PySpark Cluster',
-        LogUri='s3://aws-logs-788660014500-eu-west-1/emr-logs',
+        LogUri='s3://thousif-test123/emr-logs',
         ReleaseLabel='emr-5.30.0',
         Instances=instances,
         Configurations=configurations,
